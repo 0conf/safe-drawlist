9a357fc2eb4799d3c6c7f055e8cb659e0c8ad95d
Andrew L <45529739+hywoh@users.noreply.github.com>
2023-12-22 21:45:11 +1100

    Update SafeSwap.h

--- SafeSwap.h
+++ SafeSwap.h
@@ -1,34 +1,7 @@
 class SafeDrawlistSwap {
 public:
-
-	void Render()
-	{
-		std::unique_lock<std::shared_mutex> lock(this->m_Mutex);
-		ImGui_ImplDX11_RenderDrawData(&this->m_TempData);
-	}
-
-	void Swap(const ImDrawData* Data)
-	{
-		std::unique_lock<std::shared_mutex> lock(this->m_Mutex);
-		if (m_TempData.CmdLists != nullptr) {
-
-			for (int i = 0; i < m_TempData.CmdListsCount; i++)
-			{
-				delete m_TempData.CmdLists[i];
-			}
-
-			delete m_TempData.CmdLists;
-			m_TempData.CmdLists = nullptr;
-		}
-
-		m_TempData.Clear();
-		m_TempData = *Data;
-		m_TempData.CmdLists = new ImDrawList * [Data->CmdListsCount];
-		for (int i = 0; i < Data->CmdListsCount; i++) {
-			m_TempData.CmdLists[i] = Data->CmdLists[i]->CloneOutput();
-		}
-	}
-
+	void Render();
+	void Swap(const ImDrawData* Data);
 private:
 	std::shared_mutex m_Mutex = { };
 	ImDrawData m_TempData;
